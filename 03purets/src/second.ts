ac69026a50f6a19b41d9e008e72bf420a6e3b570
Sarvang Dave <sarvang00@gmail.com>
2023-08-28 18:10:34 -0300

    not using this for next part

--- 03purets/src/second.ts
+++ 03purets/src/second.ts
@@ -1,39 +1,39 @@
-interface TakePhoto {
-    cameraMode: string
-    filter: string
-    burst: number
-}
+// interface TakePhoto {
+//     cameraMode: string
+//     filter: string
+//     burst: number
+// }
 
-interface Story {
-    createStory(): void
-}
+// interface Story {
+//     createStory(): void
+// }
 
-class CamApp implements TakePhoto {
-    constructor(
-        public cameraMode: string,
-        public filter: string,
-        public burst: number
-    ){
-        cameraMode = "";
-        filter = "";
-        burst = 0
-    }
-}
+// class CamApp implements TakePhoto {
+//     constructor(
+//         public cameraMode: string,
+//         public filter: string,
+//         public burst: number
+//     ){
+//         cameraMode = "";
+//         filter = "";
+//         burst = 0
+//     }
+// }
 
-class CloneApp implements TakePhoto, Story {
-    constructor(
-        public cameraMode: string,
-        public filter: string,
-        public burst: number,
-        public short: string
-    ){
-        cameraMode = "";
-        filter = "";
-        burst = 0
-        short = "yea"
-    }
+// class CloneApp implements TakePhoto, Story {
+//     constructor(
+//         public cameraMode: string,
+//         public filter: string,
+//         public burst: number,
+//         public short: string
+//     ){
+//         cameraMode = "";
+//         filter = "";
+//         burst = 0
+//         short = "yea"
+//     }
 
-    createStory(): void {
-        console.log("Story created")
-    }
-}
+//     createStory(): void {
+//         console.log("Story created")
+//     }
+// }
