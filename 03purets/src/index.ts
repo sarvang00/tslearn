203415b4502472bc1585972662b7c021b5159b2b
Sarvang Dave <sarvang00@gmail.com>
2023-08-28 16:21:53 -0300

    playing with class access modifiers - public and private

--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -1,11 +1,45 @@
-class User {
-    email: string
-    name: string
-    readonly city: string = "ahm"
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = "ahm"
 
-    constructor(email: string, name: string) {
+//     constructor(email: string, name: string) {
+//         this.email=email
+//         this.name=name
+//     }
+// }
+
+class User {
+    private _courseCount = 1
+
+    readonly city: string = "ahm"
+    
+    constructor(
+        public email: string, 
+        public name: string,
+        // private userId: string
+        ){
         this.email=email
         this.name=name
+    }
+
+    private deleteToken() {
+        console.log("token deleted")
+    }
+
+    get AppleEmail(): string {
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum: number) {
+        if (courseNum<=1) {
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount = courseNum
     }
 }
 
