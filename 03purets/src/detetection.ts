2a0e496e02fa52825aead98d2ecb98ad67548421
Sarvang Dave <sarvang00@gmail.com>
2023-08-28 20:44:40 -0300

    using "in" for type narrrowing

---
+++ 03purets/src/detetection.ts
@@ -0,0 +1,44 @@
+function detectTypes(val: number | string) {
+    if (typeof val == "string") {
+        return val.toLowerCase()
+    }
+    return val + 3
+}
+
+// function provideId(id: string | null) {
+//     if(!id) {
+//         console.log("Provide the id!!");
+//         return        
+//     }
+//     id.toLowerCase()
+// }
+
+// truthy: WHAT NOT TO DO =>
+// function printAll(strs: string | string[] | null) {
+//     if (strs) {
+//         if (typeof strs === "object") {
+//             for (const s of strs) {
+//                 console.log(s);
+//             }
+//         } else if (typeof strs === "string") {
+//             console.log(strs);
+//         }
+//     }
+// }
+
+interface User {
+    name: string,
+    email: string
+}
+
+interface Admin {
+    name: string,
+    email: string,
+    isAdmin: boolean
+}
+
+function isAdminAccount(account: User | Admin) {
+    if ("isAdmin" in account) {
+        return account.isAdmin
+    }
+}
