2336656adf9c6bfb1075d044358936ae88490939
Sarvang Dave <sarvang00@gmail.com>
2023-08-28 18:09:38 -0300

    implementing interface using class

---
+++ 03purets/src/second.ts
@@ -0,0 +1,39 @@
+interface TakePhoto {
+    cameraMode: string
+    filter: string
+    burst: number
+}
+
+interface Story {
+    createStory(): void
+}
+
+class CamApp implements TakePhoto {
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst: number
+    ){
+        cameraMode = "";
+        filter = "";
+        burst = 0
+    }
+}
+
+class CloneApp implements TakePhoto, Story {
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst: number,
+        public short: string
+    ){
+        cameraMode = "";
+        filter = "";
+        burst = 0
+        short = "yea"
+    }
+
+    createStory(): void {
+        console.log("Story created")
+    }
+}
